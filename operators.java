7e38125da677b7df1568eea0f23984ddeee7f6a9
kushagra bhatnagar <kushagrabhatnagar50@gmail.com>
2019-01-11 10:08:29 +0530

    Add files via upload

---
+++ operators.java
@@ -0,0 +1,19 @@
+class operators
+{
+
+   public static void main(String args[])
+   {
+      float x,y,z,a,b,c;
+ 
+       x =5;
+      y = 10;
+      z = x + y;
+      a = x - y;
+      b = x * y;
+      c = x / y;
+      System.out.println("Sum of the integers = " + z );
+      System.out.println("diff of the integers = " + a );
+      System.out.println("mul of the integers = " + b ); 
+      System.out.println("div of the integers = " + c );
+  }
+}
